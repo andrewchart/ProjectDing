f46df067d1706e8c9a66538984f118e551feaa99
Andrew Chart <achart@hotmail.com>
2016-02-06 01:00:33 +0000

    Added ignore and index files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+//Browser key 1: AIzaSyBMiJ_zWZF9t1fq5-L-HPWBg3NcOn5ZDzY
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>GA Realtime API Test</title>
+	</head>
+	<body>
+		<article>
+			<h1>GA Realtime API Test</h1>
+			
+		</article>
+	</body>
+</html>
